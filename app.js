00a5e86cc3bf02350ba5d488a434947fdd8cc0db
SyedKumailAbbas <k214834@nu.edu.pk>
2023-03-14 00:17:40 +0500

    del functionality added

--- app.js
+++ app.js
@@ -1,20 +1,52 @@
 
-var list=document.getElementById("list")
-var add=document.getElementById("addbtn")
-var del=document.getElementById("delbtn")
-var upd=document.getElementById("updatebtn")
+var list = document.getElementById("list")
+var add = document.getElementById("addbtn")
+var del = document.getElementById("delbtn")
 
-add.setAttribute("class","btn btn-success")
-upd.setAttribute("class","btn btn-warning")
-del.setAttribute("class","btn btn-danger")
-list.setAttribute("class","list-group")
-function addlist(){
-    var a=document.getElementById("input").value
-     li=document.createElement("li")
-     li.setAttribute("class","list-group-item list-group-item-warning"); 
-    var b= document.createTextNode(a)
+list.setAttribute("class", "list-group")
+
+
+
+function addlist() {
+    var a = document.getElementById("input").value
+    li = document.createElement("li")
+    li.setAttribute("class", "list-group-item list-group-item-warning");
+    var b = document.createTextNode(a)
     li.appendChild(b)
     list.appendChild(li)
 
-    document.getElementById("input").value=" "
+    document.getElementById("input").value = " "
+    //creating remove btn
+    var delbtn = document.createElement("button")
+    var deltxt = document.createTextNode("Remove")
+    delbtn.appendChild(deltxt)
+    li.appendChild(delbtn)
+    delbtn.setAttribute("class", "btn btn-danger")
+    delbtn.setAttribute("onclick", "removeitem(this)")
+
+
+    //creating update btn
+    var updbtn = document.createElement("button")
+    var updtext = document.createTextNode("change")
+    updbtn.appendChild(updtext)
+    li.appendChild(updbtn)
+    updbtn.setAttribute("class", "btn btn-warning")
+    updbtn.setAttribute("onclick", "update(this)")
+
+
 }
+
+function removeitem(a){
+    a.parentNode.remove()
+    
+}
+
+// function update(a){
+//     var parent=a.parentNode
+//     console.log(parent)
+
+// }
+
+// function delitem(){
+
+// }
